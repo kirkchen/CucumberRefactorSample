37ba9dee73e0104f43de13615b8175ec43304b0c
Kirk Chen <rwk0119@yahoo.com.tw>
2016-10-10 23:28:50 +0800

    Refactor code

--- src/shoppingCart.ts
+++ src/shoppingCart.ts
@@ -1,19 +1,56 @@
-class ShoppingCart {
-    Calculate(memberLevel: string, price: number, qty: number): number {
-        var totalPrice = price * qty;
+enum MemberLevel {
+    VIP,
 
-        if (memberLevel === 'VIP') {
-            if (totalPrice > 500) {
-                totalPrice = totalPrice * 0.8;
-            }
-        }
-        else if (memberLevel === 'Normal') {
-            if (totalPrice > 1000 && qty > 3) {
-                totalPrice = totalPrice * 0.85;
-            }
+    Normal
+}
+
+interface IDiscounter {
+    Calculate(price: number, qty: number): number;
+}
+
+class VipDiscounter implements IDiscounter {
+    Calculate(price: number, qty: number): number {
+        let totalPrice = price * qty;
+
+        if (totalPrice > 500) {
+            return totalPrice * 0.8
         }
 
         return totalPrice;
+    }
+}
+
+class NormalDiscounter implements IDiscounter {
+    Calculate(price: number, qty: number): number {
+        let totalPrice = price * qty;
+
+        if (totalPrice > 1000 && qty > 3) {
+            return totalPrice * 0.85;
+        }
+
+        return totalPrice;
+    }
+}
+
+class DiscounterFactory {
+    static GetDiscounter(level: string) {        
+        switch (MemberLevel[level]) {
+            case MemberLevel.VIP:
+                return new VipDiscounter();            
+            case MemberLevel.Normal:
+                return new NormalDiscounter();
+        }
+    }
+}
+
+class ShoppingCart {
+    Calculate(level: string, price: number, qty: number): number {
+        var discounter = DiscounterFactory.GetDiscounter(level);
+        if(discounter){
+            return discounter.Calculate(price, qty);
+        }
+        
+        return price * qty;
     }
 }
 
